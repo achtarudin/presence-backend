93bdf17c2e6d19854c80cc9cf10503df5a731c56
achtarudin <cabe2cabean214@gmail.com>
2022-11-20 08:42:59 +0000

    fixed github

--- src/backend/app/Http/Controllers/GithubAction/GithubActionController.php
+++ src/backend/app/Http/Controllers/GithubAction/GithubActionController.php
@@ -14,24 +14,31 @@ class GithubActionController extends Controller
         $githubHash = $request->header('X-Hub-Signature');
         $localToken = config('github-action.key');
 
+        // Command to exec deploy.sh for www-data
+        $root_path = base_path();
 
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-        if (true || hash_equals($githubHash, $localHash)) {
-            $root_path = base_path();
-
-            $process = new Process([$root_path, './deploy.sh']);
-
+        $process = new Process(['./deploy.sh']);
+        $process->setWorkingDirectory($root_path);
             $process->run(function ($type, $buffer) {
-                echo '<br>';
                 echo $buffer;
-            });
+        });
+        
+        // chdir($root_path);
+        // shell_exec('./deploy.sh');
+        // dd(shell_exec('./deploy.sh'));  // ATTEMPT RUN SCRIPT
 
-            dd($root_path . '; ./deploy.sh', $process->getCommandLine());
+        // $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+        // if (true || hash_equals($githubHash, $localHash)) {
+        //     $root_path = base_path();
 
-            // $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-            // $process->run(function ($type, $buffer) {
-            //     echo $buffer;
-            // });
-        }
+        //     $process = new Process([$root_path, './deploy.sh']);
+
+        //     $process->run(function ($type, $buffer) {
+        //         echo '<br>';
+        //         echo $buffer;
+        //     });
+
+        //     dd($root_path . '; ./deploy.sh', $process->getCommandLine());
+        // }
     }
 }
